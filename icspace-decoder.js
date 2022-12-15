8577ce627e1676745ebc34926ba38a9322079708
Medad Newman <medadrufus@gmail.com>
2022-12-15 19:48:00 +0000

    linting

--- icspace-decoder.js
+++ icspace-decoder.js
@@ -8,44 +8,15 @@
 /* ******************************************
 * bytes to number
 ******************************************* */
-function readUInt8LE(bytes) {
-  return (bytes & 0xFF);
-}
-
-function readInt8LE(bytes) {
-  const ref = readUInt8LE(bytes);
-  return (ref > 0x7F) ? ref - 0x100 : ref;
-}
 
 function readUInt16LE(bytes) {
-  const value = (bytes[1] << 8) + bytes[0];
-  return (value & 0xFFFF);
+  const value = (bytes[1] << 8) + bytes[0]; // eslint-disable-line no-bitwise
+  return (value & 0xFFFF); // eslint-disable-line no-bitwise
 }
 
 function readInt16LE(bytes) {
   const ref = readUInt16LE(bytes);
-  return (ref > 0x7FFF) ? ref - 0x10000 : ref;
-}
-
-function readUInt32LE(bytes) {
-  const value = (bytes[3] << 24) + (bytes[2] << 16) + (bytes[1] << 8) + bytes[0];
-  return (value & 0xFFFFFFFF);
-}
-
-function readInt32LE(bytes) {
-  const ref = readUInt32LE(bytes);
-  return (ref > 0x7FFFFFFF) ? ref - 0x100000000 : ref;
-}
-
-function readFloatLE(bytes) {
-  // JavaScript bitwise operators yield a 32 bits integer, not a float.
-  // Assume LSB (least significant byte first).
-  const bits = bytes[3] << 24 | bytes[2] << 16 | bytes[1] << 8 | bytes[0];
-  const sign = (bits >>> 31 === 0) ? 1.0 : -1.0;
-  const e = bits >>> 23 & 0xff;
-  const m = (e === 0) ? (bits & 0x7fffff) << 1 : (bits & 0x7fffff) | 0x800000;
-  const f = sign * m * 2 ** (e - 150);
-  return f;
+  return (ref > 0x7FFF) ? ref - 0x10000 : ref;// eslint-disable-line no-bitwise
 }
 
 function decodeUplink(bytes) {
@@ -54,14 +25,14 @@ function decodeUplink(bytes) {
       longitude: readInt16LE(bytes.slice(7, 9)) * 0xffff / 1e7,
       latitude: readInt16LE(bytes.slice(5, 7)) * 0xffff / 1e7,
       altitude: readUInt16LE(bytes.slice(9, 11)),
-      sats: bytes[3] >> 3 & 0x1F,
-      pressure: ((bytes[2] >> 1) & 0x7F) * 10,
-      app_ack: bytes[2] & 0x01,
-      reset_cnt: bytes[3] & 0x7,
+      sats: bytes[3] >> 3 & 0x1F, // eslint-disable-line no-bitwise
+      pressure: ((bytes[2] >> 1) & 0x7F) * 10, // eslint-disable-line no-bitwise
+      app_ack: bytes[2] & 0x01, // eslint-disable-line no-bitwise
+      reset_cnt: bytes[3] & 0x7, // eslint-disable-line no-bitwise
       boardTemp: readInt16LE(bytes.slice(4, 5)),
-      noloadVoltage: ((bytes[0] >> 3) & 0x1F) + 18,
-      loadVoltage: (((bytes[0] << 2) & 0x1C) | ((bytes[1] >> 6) & 0x3)) + 18,
-      days_of_playback: bytes[1] & 0x3F,
+      noloadVoltage: ((bytes[0] >> 3) & 0x1F) + 18, // eslint-disable-line no-bitwise
+      loadVoltage: (((bytes[0] << 2) & 0x1C) | ((bytes[1] >> 6) & 0x3)) + 18, // eslint-disable-line no-bitwise
+      days_of_playback: bytes[1] & 0x3F, // eslint-disable-line no-bitwise
     },
   };
 }
