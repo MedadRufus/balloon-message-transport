b3237d91d1222e56f2ab04a38ede217a8b138fc9
Medad Newman <medadrufus@gmail.com>
2022-12-15 19:58:03 +0000

    linter compliance

--- icspace-decoder.js
+++ icspace-decoder.js
@@ -19,19 +19,23 @@ function readInt16LE(bytes) {
   return (ref > 0x7FFF) ? ref - 0x10000 : ref;// eslint-disable-line no-bitwise
 }
 
+function decodeLoadVoltage(byte1, byte2) {
+  return ((byte1 << 2) & 0x1C) | ((byte2 >> 6) & 0x3) + 18; // eslint-disable-line no-bitwise
+}
+
 function decodeUplink(bytes) {
   return {
     data: {
-      longitude: readInt16LE(bytes.slice(7, 9)) * 0xffff / 1e7,
-      latitude: readInt16LE(bytes.slice(5, 7)) * 0xffff / 1e7,
+      longitude: (readInt16LE(bytes.slice(7, 9)) * 0xffff) / 1e7,
+      latitude: (readInt16LE(bytes.slice(5, 7)) * 0xffff) / 1e7,
       altitude: readUInt16LE(bytes.slice(9, 11)),
-      sats: bytes[3] >> 3 & 0x1F, // eslint-disable-line no-bitwise
+      sats: (bytes[3] >> 3) & 0x1F, // eslint-disable-line no-bitwise
       pressure: ((bytes[2] >> 1) & 0x7F) * 10, // eslint-disable-line no-bitwise
       app_ack: bytes[2] & 0x01, // eslint-disable-line no-bitwise
       reset_cnt: bytes[3] & 0x7, // eslint-disable-line no-bitwise
       boardTemp: readInt16LE(bytes.slice(4, 5)),
       noloadVoltage: ((bytes[0] >> 3) & 0x1F) + 18, // eslint-disable-line no-bitwise
-      loadVoltage: (((bytes[0] << 2) & 0x1C) | ((bytes[1] >> 6) & 0x3)) + 18, // eslint-disable-line no-bitwise
+      loadVoltage: decodeLoadVoltage(bytes[0], bytes[1]),
       days_of_playback: bytes[1] & 0x3F, // eslint-disable-line no-bitwise
     },
   };
