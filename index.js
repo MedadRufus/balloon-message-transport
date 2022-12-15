c899a8869cae2fe2dbd924a977511b7c9c79cd1e
Medad Newman <medadrufus@gmail.com>
2022-12-15 21:06:28 +0000

    standardise

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-import { connect } from 'mqtt';
-
-const client = connect('mqtt://test.mosquitto.org');
-
-client.on('connect', () => {
-  client.subscribe('presence', (err) => {
-    if (!err) {
-      client.publish('presence', 'Hello mqtt');
-    }
-  });
-});
-
-client.on('message', (topic, message) => {
-  // message is Buffer
-  console.log(message.toString());
-  client.end();
-});
+module.exports = {
+  decode: require('./lib/decode').decode,
+};
