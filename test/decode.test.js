bbfea4ddc45bb8526522c82ce3ca082592429a6e
Medad Newman <medadrufus@gmail.com>
2022-12-19 20:27:18 +0000

    decode a known good string

--- test/decode.test.js
+++ test/decode.test.js
@@ -5,7 +5,10 @@ import { decode as _decode } from '../lib/decode.js';
 
 describe('decoding test suite', () => {
   it('Decode bad stripe size', () => {
-    const results = _decode([0x23, 0x23]);
+    // convert from base64 to bytearray
+    const test_string = "c4CwIQ6rHt3/VgCrHt3/cgDgOxKrHt3/WwAcPBKrHt3/cgDgOxKrHt3/WwAcPBI=";
+    const test_string_array = Buffer.from(test_string, 'base64');
+    const results = _decode(test_string_array);
     equal(26, results.sats);
   });
 });
