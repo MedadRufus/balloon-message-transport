641639fc734227efd377d6710ba0494dae5bcd66
Medad Newman <medadrufus@gmail.com>
2022-12-19 20:31:17 +0000

    successfully decode sats

--- test/decode.test.js
+++ test/decode.test.js
@@ -4,11 +4,13 @@ import { describe, it } from 'mocha';
 import { decode as _decode } from '../lib/decode.js';
 
 describe('decoding test suite', () => {
-  it('Decode bad stripe size', () => {
-    // convert from base64 to bytearray
-    const test_string = "c4CwIQ6rHt3/VgCrHt3/cgDgOxKrHt3/WwAcPBKrHt3/cgDgOxKrHt3/WwAcPBI=";
-    const test_string_array = Buffer.from(test_string, 'base64');
+
+  // convert from base64 to bytearray
+  const test_string = "c4CwIQ6rHt3/VgCrHt3/cgDgOxKrHt3/WwAcPBKrHt3/cgDgOxKrHt3/WwAcPBI=";
+  const test_string_array = Buffer.from(test_string, 'base64');
+
+  it('Decode sats', () => {
     const results = _decode(test_string_array);
-    equal(26, results.sats);
+    equal(4, results.data.sats);
   });
 });
