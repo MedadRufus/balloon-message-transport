99f3bd007074a635b305670a7dbcc2f6124a0f70
Medad Newman <medadrufus@gmail.com>
2022-12-19 20:33:02 +0000

    linter fix

--- test/decode.test.js
+++ test/decode.test.js
@@ -4,13 +4,12 @@ import { describe, it } from 'mocha';
 import { decode as _decode } from '../lib/decode.js';
 
 describe('decoding test suite', () => {
-
   // convert from base64 to bytearray
-  const test_string = "c4CwIQ6rHt3/VgCrHt3/cgDgOxKrHt3/WwAcPBKrHt3/cgDgOxKrHt3/WwAcPBI=";
-  const test_string_array = Buffer.from(test_string, 'base64');
+  const testString = 'c4CwIQ6rHt3/VgCrHt3/cgDgOxKrHt3/WwAcPBKrHt3/cgDgOxKrHt3/WwAcPBI=';
+  const testStringArray = Buffer.from(testString, 'base64');
 
   it('Decode sats', () => {
-    const results = _decode(test_string_array);
+    const results = _decode(testStringArray);
     equal(4, results.data.sats);
   });
 });
