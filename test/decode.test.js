39be0bc4903b04f2c04860f823b4c504a735174f
Medad Newman <medadrufus@gmail.com>
2022-12-20 20:18:26 +0000

    Decode solar angle (#2)
    * decode sun position
    
    * put in correct expected elevation
    
    * decode solar angle working
    
    * use approximate comparison for floats

--- test/decode.test.js
+++ test/decode.test.js
@@ -1,17 +1,18 @@
 import { equal } from 'assert';
+import { expect } from 'chai';
 import { describe, it } from 'mocha';
-
 import { decode as _decode } from '../lib/decode.js';
 
 describe('decoding test suite', () => {
   // convert from base64 to bytearray
   const testString = 'nMsAMQXxHrsAjg36HtEATgCmTxD6HtEAKADCPxD6HtEAPAAbDRD6HtEAQQDWKBA=';
+  const received_timestamp = 1641996018484;
   const testStringArray = Buffer.from(testString, 'base64');
 
   it('Decode frame', () => {
-    const results = _decode(testStringArray);
-    equal(1.2255045, results.longitude);
-    equal(51.9102735, results.latitude);
+    const results = _decode(testStringArray, received_timestamp);
+    expect(results.longitude).to.be.closeTo(1.2255045, 0.00001);
+    expect(results.latitude).to.be.closeTo(51.9102735, 0.00001);
     equal(3470, results.altitude);
     equal(6, results.sats);
     equal(0, results.pressure);
@@ -21,24 +22,25 @@ describe('decoding test suite', () => {
     equal(3.7, results.noloadVoltage);
     equal(2.1, results.loadVoltage);
     equal(11, results.days_of_playback);
+    expect(results.solarElevtationDeg).to.be.closeTo(12.15, 0.01);
 
     /* Past position altitudes */
-    equal(19.89, results.pastPositions[0].altitude);
-    equal(10.2, results.pastPositions[1].altitude);
-    equal(15.3, results.pastPositions[2].altitude);
-    equal(16.575, results.pastPositions[3].altitude);
+    expect(results.pastPositions[0].altitude).to.be.closeTo(19.89, 1);
+    expect(results.pastPositions[1].altitude).to.be.closeTo(10.2, 1);
+    expect(results.pastPositions[2].altitude).to.be.closeTo(15.3, 1);
+    expect(results.pastPositions[3].altitude).to.be.closeTo(16.575, 1);
 
     /* Past position longitudes */
-    equal(1.3696815, results.pastPositions[0].longitude);
-    equal(1.3696815, results.pastPositions[1].longitude);
-    equal(1.3696815, results.pastPositions[2].longitude);
-    equal(1.3696815, results.pastPositions[3].longitude);
+    expect(results.pastPositions[0].longitude).to.be.closeTo(1.3696815, 0.00001);
+    expect(results.pastPositions[1].longitude).to.be.closeTo(1.3696815, 0.00001);
+    expect(results.pastPositions[2].longitude).to.be.closeTo(1.3696815, 0.00001);
+    expect(results.pastPositions[3].longitude).to.be.closeTo(1.3696815, 0.00001);
 
     /* Past position latitudes */
-    equal(51.969255, results.pastPositions[0].latitude);
-    equal(51.969255, results.pastPositions[1].latitude);
-    equal(51.969255, results.pastPositions[2].latitude);
-    equal(51.969255, results.pastPositions[3].latitude);
+    expect(results.pastPositions[0].latitude).to.be.closeTo(51.969255, 0.00001);
+    expect(results.pastPositions[1].latitude).to.be.closeTo(51.969255, 0.00001);
+    expect(results.pastPositions[2].latitude).to.be.closeTo(51.969255, 0.00001);
+    expect(results.pastPositions[3].latitude).to.be.closeTo(51.969255, 0.00001);
 
     /* Past position unix timestamps */
     equal(1641978421, results.pastPositions[0].tsUnix);
