e8af93bff9676c6c72fa47c53a98fdb8e55322b0
Medad Newman <medadrufus@gmail.com>
2022-12-20 19:24:30 +0000

    simplify implementation

--- test/decode.test.js
+++ test/decode.test.js
@@ -10,40 +10,40 @@ describe('decoding test suite', () => {
 
   it('Decode frame', () => {
     const results = _decode(testStringArray);
-    equal(1.2255045, results.data.longitude);
-    equal(51.9102735, results.data.latitude);
-    equal(3470, results.data.altitude);
-    equal(6, results.data.sats);
-    equal(0, results.data.pressure);
-    equal(0, results.data.app_ack);
-    equal(1, results.data.reset_cnt);
-    equal(5, results.data.boardTemp);
-    equal(3.7, results.data.noloadVoltage);
-    equal(2.1, results.data.loadVoltage);
-    equal(11, results.data.days_of_playback);
+    equal(1.2255045, results.longitude);
+    equal(51.9102735, results.latitude);
+    equal(3470, results.altitude);
+    equal(6, results.sats);
+    equal(0, results.pressure);
+    equal(0, results.app_ack);
+    equal(1, results.reset_cnt);
+    equal(5, results.boardTemp);
+    equal(3.7, results.noloadVoltage);
+    equal(2.1, results.loadVoltage);
+    equal(11, results.days_of_playback);
 
     /* Past position altitudes */
-    equal(19.89, results.data.pastPositions[0].altitude);
-    equal(10.2, results.data.pastPositions[1].altitude);
-    equal(15.3, results.data.pastPositions[2].altitude);
-    equal(16.575, results.data.pastPositions[3].altitude);
+    equal(19.89, results.pastPositions[0].altitude);
+    equal(10.2, results.pastPositions[1].altitude);
+    equal(15.3, results.pastPositions[2].altitude);
+    equal(16.575, results.pastPositions[3].altitude);
 
     /* Past position longitudes */
-    equal(1.3696815, results.data.pastPositions[0].longitude);
-    equal(1.3696815, results.data.pastPositions[1].longitude);
-    equal(1.3696815, results.data.pastPositions[2].longitude);
-    equal(1.3696815, results.data.pastPositions[3].longitude);
+    equal(1.3696815, results.pastPositions[0].longitude);
+    equal(1.3696815, results.pastPositions[1].longitude);
+    equal(1.3696815, results.pastPositions[2].longitude);
+    equal(1.3696815, results.pastPositions[3].longitude);
 
     /* Past position latitudes */
-    equal(51.969255, results.data.pastPositions[0].latitude);
-    equal(51.969255, results.data.pastPositions[1].latitude);
-    equal(51.969255, results.data.pastPositions[2].latitude);
-    equal(51.969255, results.data.pastPositions[3].latitude);
+    equal(51.969255, results.pastPositions[0].latitude);
+    equal(51.969255, results.pastPositions[1].latitude);
+    equal(51.969255, results.pastPositions[2].latitude);
+    equal(51.969255, results.pastPositions[3].latitude);
 
     /* Past position unix timestamps */
-    equal(1641978421, results.data.pastPositions[0].tsUnix);
-    equal(1641734341, results.data.pastPositions[1].tsUnix);
-    equal(1640956321, results.data.pastPositions[2].tsUnix);
-    equal(1641382261, results.data.pastPositions[3].tsUnix);
+    equal(1641978421, results.pastPositions[0].tsUnix);
+    equal(1641734341, results.pastPositions[1].tsUnix);
+    equal(1640956321, results.pastPositions[2].tsUnix);
+    equal(1641382261, results.pastPositions[3].tsUnix);
   });
 });
